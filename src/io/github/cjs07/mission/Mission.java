86075bd35dc8179dff1b9c14e8b2ebeb06da952d
cjs07 <cjschaefer1999@gmail.com>
2013-09-22 21:43:37 -0400

    Update packages to github website and added mission/quest packages

---
+++ src/io/github/cjs07/mission/Mission.java
@@ -0,0 +1,9 @@
+package io.github.cjs07.mission;
+
+/**
+ * Created: 9/22/13 on 9:40 PM
+ * Updated: 9/22/13 at 9:40 PM
+ * Developed by CJ for the A-Shadow-In-Time project
+ */
+public abstract class Mission {
+}

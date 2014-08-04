306c95126965fd2f8320780d0b46f835f6b1b7ac
cjs07 <cjschaefer1999@gmail.com>
2014-08-04 13:14:14 -0400

    Delete Files

--- src/com/cjs07/ashadowintime/SuperComputer.java
+++
@@ -1,8 +0,0 @@
-package com.cjs07.ashadowintime;
-
-/**
- * Created by CJ on 1/25/14.
- * Developed for the A-Shadow-In-Time project.
- */
-public class SuperComputer {
-}
--- src/com/cjs07/ashadowintime/files/Save.java
+++
@@ -1,8 +0,0 @@
-package com.cjs07.ashadowintime.files;
-
-/**
- * Created by CJ on 1/27/14.
- * Developed for the A-Shadow-In-Time project.
- */
-public class Save {
-}
